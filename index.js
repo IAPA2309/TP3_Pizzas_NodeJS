06e6c17b819e773d1bad4b3a55a255d2d3d0263a
IAPA2309 <iniaki.perezaviles@gmail.com>
2023-04-28 20:45:34 -0300

    post api

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import cors from 'cors';
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // Para poder parsear el body como req.body
 app.use(cors());
 
 app.get('/pizza', async (req, res) => {
@@ -19,10 +18,17 @@ app.get('/pizza/:id', async (req, res) => {
     res.status(200).send(pizza)
 })
 
-app.post('/pizza', (req,res) => {
+
+app.use(express.json()); // Para poder parsear el body como req.body
+app.post('/pizza', async (req,res) => {
     console.log("en post, req:", req);
     try{
-        await PizzaService.insert(req.body)
+        const pizza = new Pizza()
+        pizza.nombre = req.body.nombre;
+        pizza.libreGluten = req.body.libreGluten;
+        pizza.importe = req.body.importe;
+        pizza.descripcion = req.body.descripcion;
+        await PizzaService.insert(pizza)
         res.status(200).json({ message: 'Pizza creada'});
     }catch(error){
         console.log(error);
