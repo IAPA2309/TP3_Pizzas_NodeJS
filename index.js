99f3d4263bf06e65091b5e507c233c2fdf4e6798
IAPA2309 <iniaki.perezaviles@gmail.com>
2023-04-28 22:19:54 -0300

    put y delete api pizzas

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json()); // Para poder parsear el body como req.body
 
 app.get('/pizza', async (req, res) => {
     const pizza = await PizzaService.getAll();
@@ -19,10 +20,10 @@ app.get('/pizza/:id', async (req, res) => {
 })
 
 
-app.use(express.json()); // Para poder parsear el body como req.body
 app.post('/pizza', async (req,res) => {
     console.log("en post, req:", req);
     try{
+        // Paso el json a un objeto Pizza
         const pizza = new Pizza()
         pizza.nombre = req.body.nombre;
         pizza.libreGluten = req.body.libreGluten;
@@ -33,6 +34,34 @@ app.post('/pizza', async (req,res) => {
     }catch(error){
         console.log(error);
         res.status(500).json({ error: 'Fallo el insert'});
+    }
+})
+
+app.put('/pizza/:id', async (req, res) => {
+    console.log('En put, req:', req);
+    try{
+        const pizza = new Pizza();
+        pizza.id = req.body.id;
+        pizza.nombre = req.body.nombre;
+        pizza.libreGluten = req.body.libreGluten;
+        pizza.importe = req.body.importe;
+        pizza.descripcion = req.body.descripcion;
+        await PizzaService.update(pizza)
+        res.status(200).json({ message: 'Pizza actualizada' });
+    }catch(error){
+        console.log(error);
+        res.status(200).json({ message: 'Fallo el update' });
+    }
+})
+
+app.delete('/pizza/:id', async (req, res) => {
+    console.log('En delete, req:', req);
+    try{
+        await PizzaService.deleteById(req.params.id)
+        res.status(200).json({ message: 'Pizza borrada' });
+    }catch(error){
+        console.log(error);
+        res.status(200).json({ message: 'Fallo el delete' });
     }
 })
 
